75dc0095d2eddb15a931a3a1793b9ec76d0a431e
gimcheonbapguk <simple2531@gmail.com>
2024-10-31 06:59:36 +0900

    아두이노 코드 수정
    1. 물 조절 관련 코드 제거
    2. 아두이노-파이썬 송신 메세지 제거

--- codes/python-arduino/sketch_oct12a.ino
+++ codes/python-arduino/sketch_oct12a.ino
@@ -5,29 +5,26 @@
 
 // define pin 
 #define relay 2 
-#define buzzer 3  // 부저 핀 추가 (신호 출력용)
 
 // Global variable
 unsigned long Weight = 0;
 unsigned long requiredWeight = 0;  // 파이썬에서 받은 목표 무게
 unsigned long Weight_Set=0;    //저울 영점 조절용 직전 무게
+unsigned long Weight_Before;    //저울 영점 조절용 직전 무게
 
-bool waterPoured = false;  // 물을 따랐는지 여부
 bool Weight_Flag=false;    //저울 영점 조절
 
 char msg[16] = {0x01};
 
 // 보정 상수 초기화
-float calibration_factor = 2.6;  // 필요에 따라 조정. 초기값 1.15. 1.1818
+float calibration_factor = 1.17;  // 필요에 따라 조정. 초기값 1.15. 1.1818. 2.6
 
 void setup() {
     Serial.begin(9600);
     
     // 핀 모드 설정
     pinMode(relay, OUTPUT);
-    pinMode(buzzer, OUTPUT);  // 부저 핀 추가
     digitalWrite(relay, LOW);  // 초기 상태에서 릴레이 끔
-    digitalWrite(buzzer, LOW);  // 초기 상태에서 부저 끔
 
     // Initialize libraries
     Init_HX24();  // 로드셀 라이브러리 초기화
@@ -36,16 +33,13 @@ void setup() {
 
     // LCD 초기 메시지
     for (unsigned char i = 0; i < 6; i++) Write_1621_data(5 - i, Table_Hello[i]);  // "HELLO" 표시
-    Serial.println("Setup completed.");
+    delay(3000);    //3초간 표시
 }
 
 void loop() {
     // 시리얼로부터 목표 무게 수신
     if (Serial.available() > 0) {
-        requiredWeight = Serial.parseInt();  // 파이썬에서 보낸 목표 무게 수신
-        waterPoured = false;  // 물을 따르기 시작하기 전으로 초기화
-        Serial.print("Received target weight: ");
-        Serial.println(requiredWeight);
+        //requiredWeight = Serial.parseInt();  // 파이썬에서 보낸 목표 무게 수신
     }
 
     // 현재 무게 측정 및 보정
@@ -59,18 +53,14 @@ void loop() {
     }
     Weight -= Weight_Set;
 
-    if (Weight<0)
+    if (Weight<0)   //무게가 음수일 때(오류) 0으로 설정
       Weight=0;
-    else if (Weight>5000){
+    else if (Weight>5000){    //무게가 일정 값 이상일 때 에러 메세지 출력
       Flag_Error=1;
       Weight=0;
     }
-
-    // 현재 무게 출력
-    Serial.print("Current Weight: ");
+ 
     Serial.print(Weight);
-    Serial.println(" g");
-
 
     // 세그먼트 LCD에 현재 무게 출력
     if (Flag_Error == 0) {
@@ -83,21 +73,6 @@ void loop() {
 
         sprintf(msg, "%12d g  ", Weight);
         disp_char(2,1,msg);
-
-      // 목표 무게에 도달하지 않으면 물 붓기
-      if (Weight < requiredWeight && !waterPoured) {
-          digitalWrite(relay, HIGH);  // 릴레이 켜서 물 붓기 시작
-          Serial.println("Pouring water...");
-      }
-      // 목표 무게에 도달하면 물 멈추기 및 신호 출력
-      else if (Weight > requiredWeight && !waterPoured) {
-          digitalWrite(relay, LOW);  // 물 멈추기
-          waterPoured = true;  // 물 붓기 완료 상태
-
-          // 세그먼트 LCD에 "DONE" 출력
-          disp_char(1,1,"  D O N E  ");
-          Serial.println("Water pouring done.");
-      }
     }
 
     else {
@@ -108,5 +83,5 @@ void loop() {
         disp_char(1, 1, "  E  R  R  O  R ");
     }
 
-    delay(3000);  // 10초마다 무게 측정. 소음으로 수정
+    delay(500);  // 0.5초마다 무게 측정. 소음으로 수정
 }
