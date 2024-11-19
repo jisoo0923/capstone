334fa1e72db2640fa8c3f5d85d83c7f0fe0e815a
kim0114 <168059312+kim0114@users.noreply.github.com>
2024-11-19 10:18:14 +0900

    Update uno_test01.ino

--- codes/python-arduino/uno_test01.ino
+++ codes/python-arduino/uno_test01.ino
@@ -1,3 +1,47 @@
+// 목표무게 한번 도달하면 바로 워터펌프 멈추는 코드
+
+// Include Library
+#include <I2C_LCD.h>
+#include <HX24.h>
+#include <SegmentLCD.h>
+
+// define pins for L298N motor driver
+#define in1 7  // L298N IN1 핀
+#define in2 6  // L298N IN2 핀
+
+// Global variables
+unsigned long Weight = 0;
+unsigned long Weight_Offset = 0;  // 영점 조정값
+unsigned long requiredWeight = 0; // 목표 무게
+float calibration_factor = 1.1;   // 보정 상수
+bool pumpActivated = false;       // 워터펌프 작동 여부 플래그
+bool targetReached = false;       // 목표 무게 도달 여부 플래그
+unsigned long pumpStartTime = 0;  // 워터펌프 작동 시작 시간
+unsigned long pumpDelay = 2000;   // 워터펌프 지연 시간 (2초)
+bool weightChanged = false;       // 무게가 변경되었는지 플래그
+char msg[16] = {0x01};
+
+void setup() {
+    Serial.begin(9600);
+    delay(100);
+
+    pinMode(in1, OUTPUT);
+    pinMode(in2, OUTPUT);
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+
+    Init_HX24();  
+    Init_1621();  
+    initial_lcd();  
+
+    for (unsigned char i = 0; i < 6; i++) Write_1621_data(5 - i, Table_Hello[i]);
+    Serial.println("Setup completed.");
+
+    Weight_Offset = Get_Weight();
+    Serial.print("Weight Offset: ");
+    Serial.println(Weight_Offset);
+}
+
 void loop() {
     // 시리얼로부터 목표 무게 수신
     if (Serial.available() > 0) {
